7eb0beaf0bcd870d31ed5f5fb11f187ed51c6f47
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-01-23 16:25:14 +0100

    Added BotModule

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const BotModuleManager = require('./bot-module-manager');
+
 var envConfigs = require('./environment-config.json');
 var config = envConfigs.development;
 if (process.env.NODE_ENV) {
   config = envConfigs[process.env.NODE_ENV] || envConfigs['development'];
 }
+
+const botModuleManager = new BotModuleManager(config.token);
+
+const BotModule = require('./bot-module');
+botModuleManager.registerBotModule(BotModule);
