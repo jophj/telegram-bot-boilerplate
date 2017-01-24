12c4973d83da9a133cd7c528d29c7d77a0e50aee
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-01-24 11:27:41 +0100

    Implemented 'hi' counter

--- bot-modules/sample-bot-module.js
+++ bot-modules/sample-bot-module.js
@@ -1,10 +1,25 @@
 const BotModule = require('../bot-module');
 
+function* countHi() {
+  let count = 0;
+  while (true) {
+    yield count += 1;
+  }
+}
+
 class SampleBotModule extends BotModule {
+  constructor(...args) {
+    super(...args);
+    this.hiCounter = countHi();
+  }
+
   initModule() {
-    this.bot.hears('hi', (ctx) => {
-      return ctx.reply('Hi!');
+    this.bot.hears('hi', (ctx, next) => {
+      ctx.reply(`You said 'hi' ${this.hiCounter.next().value} times`);
+      next();
     });
+
+    this.bot.on('message', (ctx) => console.log(ctx));
   }
 }
 
